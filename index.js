8edfa38b29242e5cda145f7c821a6ca71b04d1bb
paramsavla06 <paramsavla06@gmail.com>
2026-03-02 12:36:15 +0530

    add github

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/login', (req, res) => {
 	res.send("<h1>Please login at my server</h1>")
 })
 
+app.get('/github', (req, res) => {
+	res.send("<a href=\"https://github.com/paramsavla06/backend-learning\">Github link to its own repo<>")
+})
+
 app.listen(process.env.PORT, () => {
 	console.log(`Example app listening to port ${process.env.PORT}`)
 })
